1feb8d648bfb07113f53c6e0e3cea047d301d752
Marc Hermans <oriondevelopment@outlook.com>
2019-11-30 11:31:08 +0100

    Example for the transformer bug.

--- src/main/java/net/fabricmc/loom/LoomGradlePlugin.java
+++ src/main/java/net/fabricmc/loom/LoomGradlePlugin.java
@@ -24,75 +24,36 @@
 
 package net.fabricmc.loom;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Locale;
-
 import net.fabricmc.loom.transformers.ContainedZipStrippingTransformer;
 import net.fabricmc.loom.transformers.TransformerProjectManager;
+import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.Task;
-import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.attributes.Attribute;
-import org.gradle.api.internal.project.DefaultProject;
-import org.gradle.api.tasks.TaskContainer;
 
-import net.fabricmc.loom.providers.MappingsProvider;
-import net.fabricmc.loom.providers.MinecraftLibraryProvider;
-import net.fabricmc.loom.task.AbstractDecompileTask;
-import net.fabricmc.loom.task.CleanLoomBinaries;
-import net.fabricmc.loom.task.CleanLoomMappings;
-import net.fabricmc.loom.task.DownloadAssetsTask;
-import net.fabricmc.loom.task.GenEclipseRunsTask;
-import net.fabricmc.loom.task.GenIdeaProjectTask;
-import net.fabricmc.loom.task.GenVsCodeProjectTask;
-import net.fabricmc.loom.task.MigrateMappingsTask;
-import net.fabricmc.loom.task.RemapJarTask;
-import net.fabricmc.loom.task.RemapLineNumbersTask;
-import net.fabricmc.loom.task.RemapSourcesJarTask;
-import net.fabricmc.loom.task.RunClientTask;
-import net.fabricmc.loom.task.RunServerTask;
-import net.fabricmc.loom.task.fernflower.FernFlowerTask;
-
-public class LoomGradlePlugin extends AbstractPlugin {
-	private static File getMappedByproduct(Project project, String suffix) {
-		LoomGradleExtension extension = project.getExtensions().getByType(LoomGradleExtension.class);
-		MappingsProvider mappingsProvider = extension.getMappingsProvider();
-		File mappedJar = mappingsProvider.mappedProvider.getMappedJar();
-		String path = mappedJar.getAbsolutePath();
-
-		if (!path.toLowerCase(Locale.ROOT).endsWith(".jar")) {
-			throw new RuntimeException("Invalid mapped JAR path: " + path);
-		}
-
-		return new File(path.substring(0, path.length() - 4) + suffix);
-	}
-
+public class LoomGradlePlugin implements Plugin<Project>
+{
 	@Override
-	public void apply(Project target) {
-		super.apply(target);
-
-		final Attribute<Boolean> stripped = Attribute.of("stripped", Boolean.class);
+	public void apply(Project target)
+    {
+        final Attribute<Boolean> stripped = Attribute.of("stripped", Boolean.class);
 
         TransformerProjectManager.getInstance().register(target);
 
-		target.getDependencies().attributesSchema(schema -> {
-		    schema.attribute(stripped);
+        target.getDependencies().attributesSchema(schema -> {
+            schema.attribute(stripped);
         });
 
-		target.getDependencies().artifactTypes(types -> {
-		    types.all(type -> {
-		        target.getLogger().lifecycle("[Loom]: Available: " + type.getName());
+        target.getDependencies().artifactTypes(types -> {
+            types.all(type -> {
+                target.getLogger().lifecycle("[Loom]: Available: " + type.getName());
             });
 
-		    types.getByName("jar", jarType -> {
-		        jarType.getAttributes().attribute(stripped, false);
+            types.getByName("jar", jarType -> {
+                jarType.getAttributes().attribute(stripped, false);
             });
         });
 
-		target.getDependencies().registerTransform(
+        target.getDependencies().registerTransform(
           ContainedZipStrippingTransformer.class,
           config -> {
               config.getFrom().attribute(stripped, false);
@@ -103,36 +64,7 @@ public class LoomGradlePlugin extends AbstractPlugin {
           }
         );
 
-		TaskContainer tasks = target.getTasks();
-
-		tasks.register("cleanLoomBinaries", CleanLoomBinaries.class);
-		tasks.register("cleanLoomMappings", CleanLoomMappings.class);
-
-		tasks.register("cleanLoom").configure(task -> {
-			task.dependsOn(tasks.getByName("cleanLoomBinaries"));
-			task.dependsOn(tasks.getByName("cleanLoomMappings"));
-		});
-
-		tasks.register("migrateMappings", MigrateMappingsTask.class, t -> {
-			t.getOutputs().upToDateWhen((o) -> false);
-		});
-
-		tasks.register("remapJar", RemapJarTask.class);
-
-		tasks.register("genSourcesDecompile", FernFlowerTask.class, t -> {
-			t.getOutputs().upToDateWhen((o) -> false);
-		});
-
-		tasks.register("genSourcesRemapLineNumbers", RemapLineNumbersTask.class, t -> {
-			t.getOutputs().upToDateWhen((o) -> false);
-		});
-
-		tasks.register("genSources", t -> {
-			t.getOutputs().upToDateWhen((o) -> false);
-			t.setGroup("fabric");
-		});
-
-        project.getConfigurations().all(config -> {
+        target.getConfigurations().all(config -> {
             if (config.isCanBeResolved())
             {
                 config.attributes(container -> {
@@ -140,77 +72,5 @@ public class LoomGradlePlugin extends AbstractPlugin {
                 });
             }
         });
-
-        project.getLogger().lifecycle("ServiceRegistryType: " + ((DefaultProject) project).getServices().getClass().getName());
-
-		project.afterEvaluate((p) -> {
-			AbstractDecompileTask decompileTask = (AbstractDecompileTask) p.getTasks().getByName("genSourcesDecompile");
-			RemapLineNumbersTask remapLineNumbersTask = (RemapLineNumbersTask) p.getTasks().getByName("genSourcesRemapLineNumbers");
-			Task genSourcesTask = p.getTasks().getByName("genSources");
-
-			genSourcesTask.dependsOn(remapLineNumbersTask);
-			remapLineNumbersTask.dependsOn(decompileTask);
-
-			Project project = this.getProject();
-			LoomGradleExtension extension = project.getExtensions().getByType(LoomGradleExtension.class);
-			MinecraftLibraryProvider libraryProvider = extension.getMinecraftProvider().libraryProvider;
-			MappingsProvider mappingsProvider = extension.getMappingsProvider();
-			File mappedJar = mappingsProvider.mappedProvider.getMappedJar();
-			File linemappedJar = getMappedByproduct(project, "-linemapped.jar");
-			File sourcesJar = getMappedByproduct(project, "-sources.jar");
-			File linemapFile = getMappedByproduct(project, "-sources.lmap");
-
-			decompileTask.setInput(mappedJar);
-			decompileTask.setOutput(sourcesJar);
-			decompileTask.setLineMapFile(linemapFile);
-			decompileTask.setLibraries(libraryProvider.getLibraries());
-
-			remapLineNumbersTask.setInput(mappedJar);
-			remapLineNumbersTask.setLineMapFile(linemapFile);
-			remapLineNumbersTask.setOutput(linemappedJar);
-
-			Path mappedJarPath = mappedJar.toPath();
-			Path linemappedJarPath = linemappedJar.toPath();
-
-			genSourcesTask.doLast((tt) -> {
-				if (Files.exists(linemappedJarPath)) {
-					try {
-						Files.deleteIfExists(mappedJarPath);
-						Files.copy(linemappedJarPath, mappedJarPath);
-					} catch (IOException e) {
-						throw new RuntimeException(e);
-					}
-				}
-			});
-		});
-
-		tasks.register("downloadAssets", DownloadAssetsTask.class);
-
-		tasks.register("genIdeaWorkspace", GenIdeaProjectTask.class, t -> {
-			t.dependsOn("idea", "downloadAssets");
-			t.setGroup("ide");
-		});
-
-		tasks.register("genEclipseRuns", GenEclipseRunsTask.class, t -> {
-			t.dependsOn("downloadAssets");
-			t.setGroup("ide");
-		});
-
-		tasks.register("vscode", GenVsCodeProjectTask.class, t -> {
-			t.dependsOn("downloadAssets");
-			t.setGroup("ide");
-		});
-
-		tasks.register("remapSourcesJar", RemapSourcesJarTask.class);
-
-		tasks.register("runClient", RunClientTask.class, t -> {
-			t.dependsOn("assemble", "downloadAssets");
-			t.setGroup("minecraftMapped");
-		});
-
-		tasks.register("runServer", RunServerTask.class, t -> {
-			t.dependsOn("assemble");
-			t.setGroup("minecraftMapped");
-		});
-	}
+    }
 }
